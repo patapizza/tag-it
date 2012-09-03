79d15957fb7a1e20cde9245e2a74fd94385da718
patapizza <patapizza@gmail.com>
2012-09-03 15:44:38 +0200

    renaming from .ss to .rkt

--- words.rkt
+++ words.rkt
@@ -4,7 +4,7 @@
 
 (define make-counts
   (lambda ([ins (find-files (lambda (arg)
-			      (regexp-match-exact? #px".+c[a-r][0-9]{2}" arg))
+			      (regexp-match-exact? #px".+c[a][0-9]{2}" arg))
 			    (build-path (current-directory) "brown"))]
 	   [dicts '(() ())])
     (let ([dicts (get-wt (filter (lambda (arg)
@@ -17,14 +17,17 @@
 (define get-wt
   (lambda (wts dicts)
     (let* ([wt (car wts)]
-	  [dicts (list
-		   (dict-update (car dicts) wt add1 0)
-		   (dict-update (cadr dicts) (cadr (regexp-split #rx"/" wt)) add1 0))])
-      (if (null? (cdr wts))
+	   [wts (cdr wts)]
+	   [dicts (list
+		    (dict-update (car dicts) wt add1 0)
+		    (dict-update (cadr dicts) (cadr (regexp-split #rx"/" wt)) add1 0))])
+      (if (null? wts)
 	dicts
-	(get-wt (cdr wts) dicts)))))
+	(get-wt wts dicts)))))
 
-(for-each
-  (lambda (arg)
-    (display arg))
-  (dict->list (car (make-counts))))
+;(for-each
+;  (lambda (arg)
+;    (display arg))
+;  (dict->list (car (make-counts))))
+
+(display (length (cadr (make-counts))))
