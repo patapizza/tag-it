bb1236d349c888cd9065b2f04edb18a060a5bdfe
patapizza <patapizza@gmail.com>
2012-12-14 12:30:12 +0100

    adding start-of-sentence symbols

--- words.rkt
+++ words.rkt
@@ -47,6 +47,41 @@
 ; basic testing
 (displayln (get-wts))
 
+; add start-of-sentence symbols
+; @in
+;   wts: list of word/tag
+; @out
+;   wts-markers: list of word/tag with start-of-sentence symbols
+(define add-sos
+  (lambda (wts
+            [wts-markers '("<s>/<s>")])
+    (if (null? wts)
+      (flatten wts-markers)
+      (add-sos (cdr wts) (cons wts-markers 
+                                   (if (string=? (car wts) "./.")
+                                    (cons (car wts) "<s>/<s>")
+                                    (car wts)))))))
+
+; basic testing
+(displayln (add-sos (get-wts)))
+
+; filter out start-of-sentence symbols
+; @in
+;   tags-sos: list of tags with start-of-sentence symbols
+; @out
+;   tags: list of tags, without start-of-sentence symbols
+(define filter-sos
+  (lambda (tags-sos
+            [tags '()])
+    (if (null? tags-sos)
+      (flatten tags)
+      (filter-sos (cdr tags-sos) (if (string=? (car tags-sos) "<s>")
+                                   tags
+                                   (cons tags (car tags-sos)))))))
+
+; basic testing
+(displayln (filter-sos '("<s>" "vb" "nn" "jj" "<s>" "whatever" "<s>")))
+
 ; count occurrences
 ; @in
 ;   lst: a list
@@ -136,17 +171,15 @@
     (if (null? tags)
       (begin
 	(printf "(~a x ~a) matrix --> ~a\n" (dict-count tags-dict) (length words) (length (flatten matrix)))
-        (make-matrix (dict-count tags-dict) (length words) (flatten matrix)))
+        (make-matrix (- (dict-count tags-dict) 1) (length words) (flatten matrix)))
       (get-b-matrix wts-dict tags-dict words (cdr tags) (list matrix (get-b-row wts-dict words (car tags) (dict-ref tags-dict (car tags))))))))
 
 ; basic testing
 (let ([wts-dict '(("w1/t1" . 1) ("w2/t1" . 2) ("w3/t2" . 1) ("w1/t2" . 4) ("w2/t2" . 5) ("w4/t3" . 2))]
-      [tags-dict '(("t1" . 3) ("t2" . 10) ("t3" . 2))]
+      [tags-dict '(("<s>" . 1) ("t1" . 3) ("t2" . 10) ("t3" . 2))]
       [words '("w1" "w2" "w3" "w4")]
       [tags '("t1" "t2" "t3")])
   (displayln (matrix-render (get-b-matrix wts-dict tags-dict words tags))))
-
-; @todo: add start-of-sentence <s> and end-of-sentence </s> symbols
 
 ; count pairs of following tags
 ; @in
@@ -195,7 +228,7 @@
       [taglist '("t1" "t2" "t3" "t4" "t5")])
   (displayln (get-a-row tags tag tag-count taglist)))
 
-; @todo: add start and end transition probabilities for beginning and final states
+; @todo: (?) add start and end transition probabilities for beginning and final states
 
 ; build the A matrix
 ; @in
@@ -207,14 +240,14 @@
 ;   matrix: the matrix of probabilities of tags appearing
 (define get-a-matrix
   (lambda (tags-dict tags taglist
-		     [taglist-left taglist]
+		     [taglist-left (flatten (cons "<s>" taglist))]
 		     [matrix '()])
     (if (null? taglist-left)
-      (make-matrix (length taglist) (length taglist) (flatten matrix))
+      (make-matrix (+ (length taglist) 1) (length taglist) (flatten matrix))
       (get-a-matrix tags-dict tags taglist (cdr taglist-left) (list matrix (get-a-row tags (car taglist-left) (dict-ref tags-dict (car taglist-left)) taglist))))))
 
 ; basic testing
-(let ([tags-dict '(("t1" . 5) ("t2" . 3) ("t3" . 1) ("t4" . 3) ("t5" . 2))]
+(let ([tags-dict '(("<s>" . 1) ("t1" . 5) ("t2" . 3) ("t3" . 1) ("t4" . 3) ("t5" . 2))]
       [tags '("t1" "t4" "t5" "t2" "t1" "t1" "t3" "t2" "t1" "t4" "t5" "t1" "t4" "t2")]
       [taglist '("t1" "t2" "t3" "t4" "t5")])
   (displayln (matrix-render (get-a-matrix tags-dict tags taglist))))
@@ -238,7 +271,7 @@
       (let ([i (cdar tags-index)])
 	(viterbi-init a-matrix b-matrix o1
 		      (cdr tags-index)
-		      (matrix-set viterbi i 0 (* (matrix-ref a-matrix 0 i)
+		      (matrix-set viterbi (+ i 1) 1 (* (matrix-ref a-matrix 0 i)
 						 (matrix-ref b-matrix i o1))))))))
 
 ; perform the recursion step of the Viterbi algorithm
@@ -255,7 +288,7 @@
 ;   backptr: an initialized matrix, but the last col
 (define viterbi-loop
   (lambda (a-matrix b-matrix obs tags-index viterbi backptr
-		    [t 1])
+		    [t 2])
     (if (null? obs)
       (list viterbi backptr)
       (letrec ([inner-loop (lambda (vit back
@@ -264,8 +297,8 @@
 			    (list vit back)
 			    (let ([s (cdar tags-left)])
 			      (inner-loop
-			        (matrix-set vit s t (get-max vit s t a-matrix b-matrix tags-index (car obs)))
-				(matrix-set back s t (cdr (get-arg-max vit s t a-matrix tags-index)))
+			        (matrix-set vit (+ s 1) t (get-max vit s t a-matrix b-matrix tags-index (car obs)))
+				(matrix-set back (+ s 1) t (cdr (get-arg-max vit s t a-matrix tags-index)))
 				(cdr tags-left)))))]
 	    [pair (inner-loop viterbi backptr)])
 	(viterbi-loop a-matrix b-matrix (cdr obs) tags-index (car pair) (cadr pair) (+ t 1))))))
@@ -277,8 +310,8 @@
       (displayln (matrix-render vit))
       (displayln (matrix-render back))
       (list
-        (matrix-set vit s t (get-max-end vit s t a-matrix tags-index))
-	(matrix-set back s t (get-arg-max-end vit s t a-matrix tags-index))))))
+        (matrix-set vit (+ s 1) t (get-max-end vit s t a-matrix tags-index))
+	(matrix-set back (+ s 1) t (get-arg-max-end vit s t a-matrix tags-index))))))
 
 ; @todo: combine get-max and get-arg-max in one proc
 ; @todo: ditto for get-max-end and get-arg-max-end
@@ -300,8 +333,8 @@
     (if (null? tags-index)
       res
       (let* ([ss (cdar tags-index)]
-	     [cur (* (* (matrix-ref viterbi ss (- t 1))
-			(matrix-ref a-matrix ss s))
+	     [cur (* (* (matrix-ref viterbi (+ ss 1) (- t 1))
+			(matrix-ref a-matrix (+ ss 1) s))
 		     (matrix-ref b-matrix s obs))])
 	(get-max viterbi s t a-matrix b-matrix (cdr tags-index) obs (if (< res cur)
 								    cur
@@ -313,8 +346,8 @@
     (if (null? tags-index)
       res
       (let* ([ss (cdar tags-index)]
-	     [cur (* (matrix-ref viterbi ss t)
-		     (matrix-ref a-matrix ss s))])
+	     [cur (* (matrix-ref viterbi (+ ss 1) t)
+		     (matrix-ref a-matrix (+ ss 1) s))])
 	(get-max-end viterbi s t a-matrix (cdr tags-index) (if (< res cur)
 							     cur
 							     res))))))
@@ -334,10 +367,10 @@
     (if (null? tags-index)
       res
       (let* ([ss (cdar tags-index)]
-	     [cur (* (matrix-ref viterbi ss (- t 1))
-		     (matrix-ref a-matrix ss s))])
+	     [cur (* (matrix-ref viterbi (+ ss 1) (- t 1))
+		     (matrix-ref a-matrix (+ ss 1) s))])
 	(get-arg-max viterbi s t a-matrix (cdr tags-index) (if (< (car res) cur)
-							     (cons cur ss)
+							     (cons cur (+ ss 1))
 							     res))))))
 
 (define get-arg-max-end
@@ -346,10 +379,10 @@
     (if (null? tags-index)
       res
       (let* ([ss (cdar tags-index)]
-	     [cur (* (matrix-ref viterbi ss t)
+	     [cur (* (matrix-ref viterbi (+ ss 1) t)
 		     (matrix-ref a-matrix ss s))])
 	(get-arg-max-end viterbi s t a-matrix (cdr tags-index) (if (< (car res) cur)
-								 (cons cur ss)
+								 (cons cur (+ ss 1))
 								 res))))))
 
 ; @todo: address the unknown word issue
@@ -379,21 +412,23 @@
 ; @todo: optimize vars
 
 ; retrieve list of word/tag
-(let* ([wts (get-wts)]
+(let* ([wts (add-sos (get-wts))]
 ; make a dictionary word/tag -> count
        [wts-dict (list->dict wts)]
 ; extract words and tags in two lists
        [wts-exploded (explode-list wts)]
-       [words (car wts-exploded)]
-       [tags (cadr wts-exploded)]
+       [words (filter-sos (car wts-exploded))]
+       [tags-sos (cadr wts-exploded)]
 ; make a dictionary tag -> count
-       [tags-dict (list->dict tags)]
-; retrieve its size
-       [tags-size (dict-count tags-dict)]
+       [tags-dict (list->dict tags-sos)]
+; retrieve its size, minus start-of-sentence symbol
+       [tags-size (- (dict-count tags-dict) 1)]
 ; filter out duplicates of words list
        [words-unique (filter-unique words)]
 ; retrieve its size
        [words-size (length words-unique)]
+; remove start-of-sentence symbols
+       [tags (filter-sos tags-sos)]
 ; filter out duplicates of tags list
        [tags-unique (filter-unique tags)]
 ; build tags index (make-hash)?
@@ -401,23 +436,26 @@
 ; build words index
        [words-index (make-hash (index-list words-unique))]
 ; build A matrix
-       [a-matrix (get-a-matrix tags-dict tags tags-unique)]
+       [a-matrix (get-a-matrix tags-dict tags-sos tags-unique)]
 ; build B matrix
        [b-matrix (get-b-matrix wts-dict tags-dict words tags-unique)]
 ; the observations (words) to tag
-       [obs '("I" "want" "to" "try" "it")]
+       ;[obs '("I" "want" "to" "try" "it")]
+       [obs '("I" "to" "it")]
 ; number of rows of viterbi and backptr matrices
        [n (+ (length tags-unique) 2)]
 ; number of cols of viterbi and backptr matrices
-       [m (+ (length obs) 1)]
+       [m (+ (length obs)  1)]
 ; 0-filled list to populate viterbi and backptr matrices
        [lst-0 (build-list (* n m) (lambda (x) 0))])
     (printf "a-matrix: ~a\n b-matrix: ~a\n" (matrix-render a-matrix) (matrix-render b-matrix))
+    (printf "vit-init: ~a\n" (matrix-render (viterbi-init a-matrix b-matrix (hash-ref words-index (car obs)) tags-index (make-matrix n m lst-0))))
+    (printf "I: ~a want: ~a to: ~a try: ~a it ~a\n" (hash-ref words-index "I" -1) (hash-ref words-index "want" -1) (hash-ref words-index "to" -1) (hash-ref words-index "try" -1) (hash-ref words-index "it" -1))
     (displayln (matrix-render (car (viterbi-end
 				     (viterbi-loop
 				       a-matrix
 				       b-matrix
-				       (index-words obs words-index)
+				       (index-words (cdr obs) words-index)
 				       tags-index
 				       (viterbi-init
 				         a-matrix
